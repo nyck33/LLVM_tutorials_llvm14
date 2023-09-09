6f6441c8bcd074f757c0f587cd971cdcf1f3fbf8
nyck33 <nobutaka@gatech.edu>
2023-09-10 08:45:47 +0900

    moving on to code gen to output llvm ir

---
+++ build/CMakeFiles/toy.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/toy.dir/toy.cpp.o"
+  "CMakeFiles/toy.dir/toy.cpp.o.d"
+  "toy"
+  "toy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/toy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
